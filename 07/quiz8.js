13d0346e16d47f98caef6e3d0cf39521fe93a13c
hijihyun <67703614+hijihyun@users.noreply.github.com>
2021-05-12 03:23:36 +0900

    07 수업 과제

--- 07/quiz8.js
+++ 07/quiz8.js
@@ -3,24 +3,24 @@ let $min = document.getElementById('min');
 let $min = document.getElementById('min');
 let $btn = document.getElementById('btn');
 
-function random() {
-	let randomNumber = Math.random() * 380;
-	$bug.style.left = randomNumber + 'px';
-	$bug.style.top = randomNumber + 'px';
-}
+// function random() {
+// 	let randomNumber = Math.random() * 380;
+// 	$bug.style.left = randomNumber + 'px';
+// 	$bug.style.top = randomNumber + 'px';
+// }
 
-$bug.addEventListener('click', (event) => {
-	$point.innerHTML++;
-	$life.innerHTML++;
-});
+// $bug.addEventListener('click', (event) => {
+// 	$point.innerHTML++;
+// 	$life.innerHTML++;
+// });
 
-$box.addEventListener('click', (event) => {
-	if ($life.innerHTML === '1') {
-		alert('gameover');
-		location.reload(true);
-		return;
-	}
-	$life.innerHTML--;
-});
+// $box.addEventListener('click', (event) => {
+// 	if ($life.innerHTML === '1') {
+// 		alert('gameover');
+// 		location.reload(true);
+// 		return;
+// 	}
+// 	$life.innerHTML--;
+// });
 
-setInterval(random, 2000);
+// setInterval(random, 2000);
