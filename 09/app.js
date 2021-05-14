90ea61f7e975d761449beab10605f7c4e8bf4d80
hijihyun <67703614+hijihyun@users.noreply.github.com>
2021-05-14 18:26:30 +0900

    quiz9

--- 09/app.js
+++ 09/app.js
@@ -1,14 +1,14 @@
 // fetch 이용해 http로 데이터 가져오기
 /*
 const promise = fetch('http');
-console.log(promise);
+console.log(promise); // Promise 객체 리턴
 
 promise.then((res) => {
-	console.log('res:', res);
-	const testPromise = res.text();
+	console.log('res:', res); // Response 정보
+	const testPromise = res.text(); // 텍스트로
 	console.log('testPromise:', testPromise);
 
-	testPromise.then((data) => console.log('data:', data));
+	testPromise.then((data) => console.log('data:', data)); // 한번 더 then
 });
 */
 
--- 09/quiz9.js
+++ 09/quiz9.js
@@ -1,10 +1,29 @@
-const $url = document.getElementById('url').value;
+const $url = document.getElementById('url');
 const $btn = document.getElementById('btn');
-const $log = document.getElementById('log').value;
+const $log = document.getElementById('log');
 
-$btn.addEventListener('click', (event) => {
-	if ($url === '') {
+function fetchUrl() {
+	const url = document.getElementById('url').value;
+	if (url === '') {
 		alert('url을 입력해주세요.');
 		return;
+	}
+
+	const promise = fetch(url);
+	let promiseCatch = fetch('https://daum.net');
+
+	promise.then((res) => {
+		const testPromise = res.text();
+		testPromise
+			.then((data) => ($log.value = data))
+			.catch(promiseCatch.then((data) => ($log.value = data)));
+	});
+}
+
+$btn.addEventListener('click', fetchUrl);
+
+$url.addEventListener('keydown', (event) => {
+	if (event.keyCode == 13) {
+		fetchUrl();
 	}
 });
